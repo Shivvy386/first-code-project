fa9906c57093b929d8e033a3d2778c63bec0aa19
Shivvy386 <sibaneesatapathy77@gmail.com>
2026-08-24 23:06:23 +0530

    Create meal.py

---
+++ week 1/meal.py
@@ -0,0 +1,13 @@
+time = input("What time is it? ")
+
+hours, minutes = time.split(":")
+time = float(hours) + float(minutes) / 60
+
+if 7 <= time <= 8:
+    print("breakfast time")
+
+elif 12 <= time <= 13:
+    print("lunch time")
+
+elif 18 <= time <= 19:
+    print("dinner time")
